163856d5c478a70a87bf24e00fc7e9bc2c6085ab
Clint Kingston <ckingston1@woolworths.com.au>
2025-06-11 00:10:27 +1000

    footer component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { HeaderComponent } from 'src/components/header/header.component';
 import { Config } from './app.config';
+import { FooterComponent } from 'src/components/footer/footer.component';
 
 @Component({
-  imports: [RouterModule, HeaderComponent],
+  imports: [RouterModule, HeaderComponent, FooterComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
