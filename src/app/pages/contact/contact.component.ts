b96018f28de43e80ad3728dd2e77b3cf336e5a20
Clint Kingston <ckingston1@woolworths.com.au>
2025-06-11 08:30:02 +1000

    contact page and footer disclaimer

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-contact',
-  imports: [CommonModule],
+  imports: [CommonModule, MatIconModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css',
 })
