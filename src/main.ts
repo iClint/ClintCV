b34f0aac40b14c794ada72a7434c60a929f2a7fc
Clint Kingston <ckingston1@woolworths.com.au>
2025-06-20 16:19:44 +1000

    404 redirect for SPA pages

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,12 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
+const redirectPath = sessionStorage.getItem('redirectPath');
+if (redirectPath) {
+  sessionStorage.removeItem('redirectPath');
+  history.replaceState(null, '', redirectPath);
+}
+
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
   console.error(err)
 );
