4ba033f703f6030a16e6166e88a4a5a1ea75a3f5
Clint Kingston <ckingston1@woolworths.com.au>
2025-07-03 10:09:40 +1000

    fix test

--- src/app/pages/projects/projects.component.spec.ts
+++ src/app/pages/projects/projects.component.spec.ts
@@ -1,13 +1,13 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ProjectsComponent } from './projects.component';
-
+import { RouterTestingModule } from '@angular/router/testing';
 describe('ProjectsComponent', () => {
   let component: ProjectsComponent;
   let fixture: ComponentFixture<ProjectsComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ProjectsComponent],
+      imports: [ProjectsComponent, RouterTestingModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ProjectsComponent);
