a9714a39beb35797025da2ff9f1ca135de09519b
Clint Kingston <ckingston1@woolworths.com.au>
2025-07-03 10:11:22 +1000

    fix deprecated module

--- src/app/pages/projects/projects.component.spec.ts
+++ src/app/pages/projects/projects.component.spec.ts
@@ -1,13 +1,14 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ProjectsComponent } from './projects.component';
-import { RouterTestingModule } from '@angular/router/testing';
+import { provideRouter } from '@angular/router';
 describe('ProjectsComponent', () => {
   let component: ProjectsComponent;
   let fixture: ComponentFixture<ProjectsComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ProjectsComponent, RouterTestingModule],
+      imports: [ProjectsComponent],
+      providers: [provideRouter([])],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ProjectsComponent);
