e03ec3ea2de523c497a44f1174ec58c32ab5eec1
Clint Kingston <ckingston1@woolworths.com.au>
2025-07-03 10:45:32 +1000

    project portfolio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { AboutComponent } from './pages/about/about.component';
 import { ProjectsComponent } from './pages/projects/projects.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { ProjectOtpComponent } from './pages/projects/project-otp/project-otp.component';
+import { ProjectPortfolioComponent } from './pages/projects/project-portfolio/project-portfolio.component';
 
 export const appRoutes: Route[] = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
@@ -12,5 +13,6 @@ export const appRoutes: Route[] = [
   { path: 'about', component: AboutComponent },
   { path: 'projects', component: ProjectsComponent },
   { path: 'projects/project-otp', component: ProjectOtpComponent },
+  { path: 'projects/project-portfolio', component: ProjectPortfolioComponent },
   { path: 'contact', component: ContactComponent },
 ];
---
+++ src/app/pages/projects/project-portfolio/project-portfolio.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-project-portfolio',
+  imports: [CommonModule],
+  templateUrl: './project-portfolio.component.html',
+  styleUrl: './project-portfolio.component.css',
+})
+export class ProjectPortfolioComponent {}
