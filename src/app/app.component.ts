c24e3974593471a805ce39fa09b26c3c4bc61179
Clint Kingston <ckingston1@woolworths.com.au>
2025-07-28 15:07:09 +1000

    cloudflare analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { NavigationEnd, Router, RouterModule } from '@angular/router';
 import { HeaderComponent } from 'src/components/header/header.component';
 import { Config } from './app.config';
 import { FooterComponent } from 'src/components/footer/footer.component';
+import { filter } from 'rxjs';
 
 @Component({
   imports: [RouterModule, HeaderComponent, FooterComponent],
@@ -12,4 +13,16 @@ import { FooterComponent } from 'src/components/footer/footer.component';
 })
 export class AppComponent {
   config = Config;
+  constructor(private router: Router) {
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe(() => {
+        if ('cf_beacon' in window) {
+          // Force Cloudflare to re-send analytics beacon
+          (window as any).cf_beacon &&
+            (window as any).cf_beacon.ping &&
+            (window as any).cf_beacon.ping();
+        }
+      });
+  }
 }
