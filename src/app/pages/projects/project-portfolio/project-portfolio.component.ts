15fad1fd9bd82b19bb39fb88afb643a6fc373e52
Clint Kingston <ckingston1@woolworths.com.au>
2025-07-28 12:08:19 +1000

    additional portfolio details and fixes

--- src/app/pages/projects/project-portfolio/project-portfolio.component.ts
+++ src/app/pages/projects/project-portfolio/project-portfolio.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TickerComponent } from 'src/components/ticker/ticker.component';
 
 @Component({
   selector: 'app-project-portfolio',
-  imports: [CommonModule],
+  imports: [CommonModule, TickerComponent],
   templateUrl: './project-portfolio.component.html',
   styleUrl: './project-portfolio.component.css',
 })
-export class ProjectPortfolioComponent {}
+export class ProjectPortfolioComponent {
+  tickerConfig = {
+    items: [
+      'Welcome to my portfolio!',
+      'Check out my latest projects.',
+      'I love building with Angular!',
+    ],
+  };
+}
