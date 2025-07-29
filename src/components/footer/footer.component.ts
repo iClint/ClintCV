ccfdd492c6943ffb03ccc9b22b2f064fa1a1d409
Clint Kingston <ckingston1@woolworths.com.au>
2025-07-29 10:03:08 +1000

    Various fixes

--- src/components/footer/footer.component.ts
+++ src/components/footer/footer.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FooterConfig } from './footer.config';
 
 @Component({
   selector: 'app-footer',
@@ -7,4 +8,17 @@ import { CommonModule } from '@angular/common';
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css',
 })
-export class FooterComponent {}
+export class FooterComponent {
+  config = FooterConfig;
+  get currentYear(): number {
+    return new Date().getFullYear();
+  }
+
+  get disclaimer(): string {
+    return this.config.disclaimer;
+  }
+
+  get buildFooterText(): string {
+    return `${this.config.copyright} ${this.currentYear} ${this.config.name} - ${this.config.reserved}`;
+  }
+}
---
+++ src/components/footer/footer.config.ts
@@ -0,0 +1,6 @@
+export const FooterConfig = {
+  copyright: `©`,
+  name: `Clint Kingston`,
+  reserved: `All rights reserved.`,
+  disclaimer: `Opinions expressed are solely my own and do not represent my employer or clients. All screenshots use mock data—no real customer information is displayed.`,
+};
