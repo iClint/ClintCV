a5dcb176d52d9981ab2e559803c71cdf438913e9
Clint Kingston <ckingston1@woolworths.com.au>
2025-07-29 14:32:12 +1000

    header unit tests

--- src/components/header/header.component.spec.ts
+++ src/components/header/header.component.spec.ts
@@ -8,9 +8,8 @@ describe('HeaderComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HeaderComponent]
-    })
-    .compileComponents();
+      imports: [HeaderComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
@@ -19,5 +18,28 @@ describe('HeaderComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render header content', () => {
+    component.title = 'website title';
+    component.subtitle = 'website subtitle';
+    component.description = 'website description';
+    fixture.detectChanges();
+
+    const headerTitle = fixture.nativeElement.querySelector('.header__title');
+    const headerSubtitle =
+      fixture.nativeElement.querySelector('.header__subtitle');
+    const headerDescription = fixture.nativeElement.querySelector(
+      '.header__description'
+    );
+
+    expect(headerTitle.textContent).toEqual('website title');
+    expect(headerSubtitle.textContent).toEqual('website subtitle');
+    expect(headerDescription.textContent).toEqual('website description');
+  });
+
+  it('should render the nav menu', () => {
+    const navMenu = fixture.nativeElement.querySelector('app-nav-menu');
+    expect(navMenu).toBeTruthy();
   });
 });
